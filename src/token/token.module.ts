773c03e346d592d836bbda420450fc807a67bae1
E-m-i-n-e-n-c-e <kssakhilraj@gmail.com>
2026-01-31 15:10:35 +0530

    token expiry change

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -14,7 +14,7 @@ import { RolesGuard } from './guards/roles.guard';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '2h' },
+        signOptions: { expiresIn: '1h' },
       }),
     }),
   ],
